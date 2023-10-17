0ebfff8d15f2df37df2f349bd51d7c0997977755
nikhil7761 <nikhilvargude@gmail.com>
2023-10-17 11:16:50 +0530

    udates

---
+++ DemoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace details
+{
+
+    class addition{
+
+
+        public static void Main(string [] args){
+        Console.WriteLine("Hello, World!");
+         
+        int[] car=new int[10]{12,34,11,24,14,56,32,76,34,65};
+        Console. WriteLine(car[2]);
+
+
+        }
+    }
+
+}
